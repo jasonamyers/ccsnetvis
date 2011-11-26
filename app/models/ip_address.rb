456eefb834b64fd6f1a7434b6d96e7c68082c992
Jason Myers <jason@jasonamyers.com>
2011-11-26 11:13:04 -0600

    Added Sites and Contact Scaffold

--- app/models/ip_address.rb
+++ app/models/ip_address.rb
@@ -1,2 +1,8 @@
 class IpAddress < ActiveRecord::Base
+	validates :address, :name, presence: true
+	validates :address, uniqueness: true
+	validates :name, format: {
+		with: %r{\.com$}i,
+		message: 'must be a FQDN'
+	}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Ccsnetvis::Application.routes.draw do
+  resources :contacts
+
+  resources :sites
+
+  get "info/index"
+
   resources :ip_addresses
 
   # The priority is based upon order of creation:
@@ -51,6 +57,7 @@ Ccsnetvis::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root to: 'info#index', as: 'info'
 
   # See how all your routes lay out with "rake routes"
 
